cac2005e06e0be7121e55c9e7c948dceee75c8b1
Rahul <rahulguptaji1019@gmail.com>
2024-09-08 16:20:01 +0530

    Added proxy

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,19 +1,28 @@
 import axios from "axios"
 import express  from "express"
+import dotenv from "dotenv"
+import url from "url"
+
+
+dotenv.config()
 
 export const router = express.Router()
 const API_URL = process.env.API_URL
-const API_KEY = process.env.API_KEY
-const API_NAME = process.env.API_NAME
+const API_KEY : string | any = process.env.API_KEY
+const API_NAME : string | any = process.env.API_NAME 
+console.log(API_NAME)
 
-router.get("/api", async (req,res)=>{
+router.get("/", async (req,res)=>{
     try {
-        const response = await axios.get(`${API_URL}`)
+            const params = new URLSearchParams({
+                [API_NAME] : API_KEY,
+                ...url.parse(req.url, true).query
+            })
+
+        const response = await axios.get(`${API_URL}?${params}`)
         const data = response.data
     
-         return res.status(200).json({
-            data
-         })    
+         return res.status(200).json({data})    
     } catch (error) {
         console.log(error)
         return res.status(500).json({
@@ -22,15 +31,3 @@ router.get("/api", async (req,res)=>{
     } 
     
 })
-
-
-
-
-
-
-
-
-
-
-
-
