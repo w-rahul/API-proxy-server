a0660f16706935938edd35f5191d63f253e7aa83
Rahul <rahulguptaji1019@gmail.com>
2024-09-08 16:20:17 +0530

    Added rate limiting

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,22 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import { router } from "./routes/api.route"
+import ratelimit from "express-rate-limit"
+
 
 dotenv.config()
 
 const app = express()
 const PORT =  process.env.PORT
 app.use(cors())
+
+const limiter = ratelimit({
+    windowMs : 10 * 60 * 1000,
+    max : 5
+})
+
+app.use(limiter)
+app.set('trust proxy', 1)
 
 app.use("/api", router)
 
